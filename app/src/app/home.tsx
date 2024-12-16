0f8b8df2b493f715c2c5ab7d535c6ae9e0b3056f
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-12-16 00:06:37 -0300

    mobile adicionado

---
+++ app/src/app/home.tsx
@@ -0,0 +1,30 @@
+import { Alert, View } from "react-native";
+import { api } from "@/services/api";
+import { Categories, CategoriesProps } from "@/components/categories";
+import { useEffect, useState } from "react";
+
+export default function Home() {
+  const [categories, setCategories] = useState<CategoriesProps>([]);
+  const [category, setCategory] = useState("")
+
+  async function fetchCategories() {
+    try {
+      const { data } = await api.get("/categories");
+      setCategories(data);
+      setCategory(data[0].id)
+    } catch (error) {
+      console.log(error);
+      Alert.alert("categorias", "não foi possível carregar as categorias");
+    }
+  }
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Categories data={categories} onSelect={setCategory} selected={category} />
+    </View>
+  );
+}
