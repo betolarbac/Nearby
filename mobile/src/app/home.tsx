f0bca818b8301761361b64b7e45d74133a20bbee
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-12-17 01:56:52 -0300

    fetch de lojas com cupons baseado na categoria e atualização com useEfeect estilização de bottomSheet

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -2,19 +2,42 @@ import { Alert, View } from "react-native";
 import { api } from "@/services/api";
 import { Categories, CategoriesProps } from "@/components/categories";
 import { useEffect, useState } from "react";
+import { PlaceProps } from "@/components/place";
+import { Places } from "@/components/places";
+
+
+type MarketsProps = PlaceProps & {};
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
-  const [category, setCategory] = useState("")
+  const [category, setCategory] = useState("");
+  const [markets, setMarkets] = useState<MarketsProps[]>([]);
 
   async function fetchCategories() {
     try {
       const { data } = await api.get("/categories");
       setCategories(data);
-      setCategory(data[0].id)
+      setCategory(data[0].id);
     } catch (error) {
       console.log(error);
       Alert.alert("categorias", "não foi possível carregar as categorias");
+    }
+  }
+
+  async function fetchMarkets() {
+    try {
+      if (!category) {
+        return;
+      }
+
+      const { data } = await api.get("/markets/category/" + category);
+      setMarkets(data);
+    } catch (error) {
+      console.log(error);
+      Alert.alert(
+        "estabelecimentos",
+        "não foi possível carregar os estabelecimentos"
+      );
     }
   }
 
@@ -22,9 +45,19 @@ export default function Home() {
     fetchCategories();
   }, []);
 
+  useEffect(() => {
+    fetchMarkets();
+  }, [category]);
+
   return (
-    <View style={{ flex: 1 }}>
-      <Categories data={categories} onSelect={setCategory} selected={category} />
+    <View style={{ flex: 1, backgroundColor: "#cecece" }}>
+      <Categories
+        data={categories}
+        onSelect={setCategory}
+        selected={category}
+      />
+
+      <Places data={markets}/>
     </View>
   );
 }
