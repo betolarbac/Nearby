12eabec5a654512ab4e43e3d6e251dc17517c410
Roberto Cabral <roberto.cabral@peopleinteractive.com.br>
2024-12-20 00:20:43 -0300

    feat: mapa e localização adicionada pegando localização do usuario e das lojas

--- mobile/src/app/home.tsx
+++ mobile/src/app/home.tsx
@@ -1,12 +1,20 @@
-import { Alert, View } from "react-native";
+import { Alert, View, Text } from "react-native";
+import MapView, { Callout, Marker } from "react-native-maps";
 import { api } from "@/services/api";
 import { Categories, CategoriesProps } from "@/components/categories";
 import { useEffect, useState } from "react";
 import { PlaceProps } from "@/components/place";
 import { Places } from "@/components/places";
+import { fontFamily, colors } from "@/styles/theme";
+type MarketsProps = PlaceProps & {
+  latitude: number;
+  longitude: number;
+};
 
-
-type MarketsProps = PlaceProps & {};
+const currentLocation = {
+  latitude: -23.561187293883442,
+  longitude: -46.656451388116494,
+};
 
 export default function Home() {
   const [categories, setCategories] = useState<CategoriesProps>([]);
@@ -56,8 +64,62 @@ export default function Home() {
         onSelect={setCategory}
         selected={category}
       />
+      <MapView
+        style={{ flex: 1 }}
+        initialRegion={{
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+        
+      >
+        <Marker
+          identifier="current"
+          coordinate={{
+            latitude: currentLocation.latitude,
+            longitude: currentLocation.longitude,
+          }}
+          image={require("@/assets/location.png")}
+        />
 
-      <Places data={markets}/>
+        {markets.map((item) => (
+          <Marker
+            key={item.id}
+            identifier={item.id}
+            coordinate={{
+              latitude: item.latitude,
+              longitude: item.longitude,
+            }}
+            image={require("@/assets/pin.png")}
+          >
+            <Callout>
+              <View>
+                <Text
+                  style={{
+                    fontSize: 14,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.medium,
+                  }}
+                >
+                 Teste
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 12,
+                    color: colors.gray[600],
+                    fontFamily: fontFamily.regular,
+                  }}
+                >
+                  {item.address}
+                </Text>
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+      </MapView>
+
+      <Places data={markets} />
     </View>
   );
 }
--- mobile/src/components/category/style.ts
+++ mobile/src/components/category/style.ts
@@ -21,9 +21,9 @@ export const s = StyleSheet.create({
   },
   containerSelected: {
     backgroundColor: colors.green.base,
-    borderWidth: 0,
+    borderColor: colors.green.base,
   },
   nameSelected: {
-    color: colors.gray[100]
+    color: colors.gray[100],
   },
 });
